2aa32b98ed69834925febc6fd0fb38d15365aedf
James Tunnell <jamestunnell@gmail.com>
2014-09-01 08:02:29 -0700

    Add tests for non-negative integers.

--- spec/parsing/numbers/positive_integer_spec.rb
+++ spec/parsing/numbers/positive_integer_spec.rb
@@ -1,27 +1,17 @@
 require File.expand_path(File.dirname(__FILE__) + '/../../spec_helper')
 
-describe PositiveInteger do
-  before :all do
-    @parser = PositiveIntegerParser.new
-  end
-  
-  it 'should parse "1"' do
-    @parser.parse("1").should_not be nil
+describe PositiveIntegerParser do
+  parser = PositiveIntegerParser.new
+
+  ["1","50","05","502530"].each do |str|
+    it "should parse '#{str}'" do
+      parser.parse(str).should_not be nil
+    end
   end
 
-  it 'should parse "50"' do
-    @parser.parse("50").should_not be nil
-  end
-
-  it 'should parse "05"' do
-    @parser.parse("50").should_not be nil
-  end
-
-  it 'should parse "502530"' do
-    @parser.parse("50").should_not be nil
-  end
-
-  it 'should not parse "0"' do
-    @parser.parse("0").should be nil
+  ["0"].each do |str|
+    it "should not parse '#{str}'" do
+      parser.parse(str).should be nil
+    end
   end
 end
